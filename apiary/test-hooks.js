6b2b3cdb9ba4bf5dd8fbfb0d8d659fff9bd04385
Boris Bügling <boris@icculus.org>
2016-07-21 15:40:13 +0200

    Workaround linting bug with `_` in parameter value
    The `sync_token` we are using contains an underscore, but those are
    flagged by the blueprint linter. As a workaround, we keep `--` in the
    blueprint and replace it in a `beforeEach` hook.

--- apiary/test-hooks.js
+++ apiary/test-hooks.js
@@ -6,3 +6,11 @@ var delay = 1000/6.0;
 hooks.afterEach(function (transaction, done) {
   setTimeout(done, delay);
 });
+
+hooks.beforeEach(function (transaction) {
+  // _ in URI parameter values raises a blueprint linter warning
+  var path = transaction.fullPath;
+  if (/sync_token/.test(path)) {
+    transaction.fullPath = path.replace(/--/, '_');
+  }
+});
